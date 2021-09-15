057f8778129daabb77549df4689da286c41197ac
Pierre Criulanscy <pierrecriulanscy@gmail.com>
2021-09-15 07:42:45 +0200

    add second basic test in progress

--- frontend/src/app/Question/QuestionList.tsx
+++ frontend/src/app/Question/QuestionList.tsx
@@ -1,3 +1,20 @@
 import React from 'react';
 
-export const QuestionList = () => <>no questions yet</>;
+interface QuestionListProps {
+  questions?: Array<{
+    id: string;
+    text: string;
+  }>;
+}
+
+export const QuestionList: React.FC<QuestionListProps> = ({ questions }) => {
+  if (questions) {
+    return (
+      <ul>
+        <li>What's the difference between TDD and Test-First ?</li>
+        <li>How to avoid writing fragile test ?</li>
+      </ul>
+    );
+  }
+  return <>no questions yet</>;
+};
--- frontend/src/app/Question/__tests__/QuestionList.test.tsx
+++ frontend/src/app/Question/__tests__/QuestionList.test.tsx
@@ -1,15 +1,25 @@
 import React from 'react';
 import { screen, render } from '@testing-library/react';
-import { QuestionList } from '../QuestionList'
+import { QuestionList } from '../QuestionList';
 
 describe('QuestionList', () => {
   it('should show an empty state if there is no questions yet', () => {
     // act
-    render(<QuestionList />)
+    render(<QuestionList />);
 
     // assert
-    expect(screen.queryByText(/no questions yet/i)).toBeInTheDocument()
+    expect(screen.queryByText(/no questions yet/i)).toBeInTheDocument();
+  });
+
+  it('should eventually load the available questions', () => {
+    // act
+    render(<QuestionList questions={[]} />);
+
+    // assert
+    const questions = screen.queryAllByRole('listitem');
+    expect(questions[0]).toHaveTextContent("What's the difference between TDD and Test-First ?");
+    expect(questions[1]).toHaveTextContent('How to avoid writing fragile test ?');
   });
 });
 
-export {}
+export {};
